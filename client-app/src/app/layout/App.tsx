b2654f3fa919d0b6f4c0e946fd4c827e4db1606e
MateuscMachado <mateuscalabria@gmail.com>
2021-09-23 16:53:08 -0300

    heroku deploy

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react';
-import './styles.css';
-import { Container } from 'semantic-ui-react';
-import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
 import { Route, Switch, useLocation } from 'react-router';
-import HomePage from '../../features/home/HomePage';
-import ActivityForm from '../../features/activities/form/ActivityForm';
-import ActivityDetails from '../../features/activities/details/ActivityDetails';
-import TestErrors from '../../features/errors/TestErrors';
 import { ToastContainer } from 'react-toastify';
+import { Container } from 'semantic-ui-react';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 import NotFound from '../../features/errors/NotFound';
-import LoginForm from '../../features/users/LoginForm';
+import TestError from '../../features/errors/TestError';
+import HomePage from '../../features/home/HomePage';
+import ProfilePage from '../../features/profiles/ProfilePage';
+import ServerError from '../../features/errors/ServerError';
+import ModalContainer from '../common/modals/ModalContainer';
 import { useStore } from '../stores/store';
 import LoadingComponent from './LoadingComponent';
-import ServerError from '../../features/ServerError';
-import ModalContainer from '../common/modals/ModalContainer';
-import ProfilePage from '../../features/profiles/ProfilePage';
+import NavBar from './NavBar';
+import PrivateRoute from './PrivateRoute';
+import './styles.css';
 
 function App() {
   const location = useLocation();
@@ -44,13 +44,12 @@ function App() {
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
               <Switch>
-                <Route exact path='/activities' component={ActivityDashboard} />
-                <Route path='/activities/:id' component={ActivityDetails} />
-                <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-                <Route path='/profiles/:username' component={ProfilePage} />
-                <Route path='/errors' component={TestErrors} />
+                <PrivateRoute exact path='/activities' component={ActivityDashboard} />
+                <PrivateRoute path='/activities/:id' component={ActivityDetails} />
+                <PrivateRoute key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+                <PrivateRoute path='/profiles/:username' component={ProfilePage} />
+                <PrivateRoute path='/errors' component={TestError} />
                 <Route path='/server-error' component={ServerError} />
-                <Route path='/login' component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -10,19 +10,17 @@ import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 export const history = createBrowserHistory();
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
     <Router history={history}>
+      <ScrollToTop />
       <App />
     </Router>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
